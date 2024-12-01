557906b4eb714e5382240a0ede4ec3ed984f7f54
kieranochola <ocholadavidk@gmail.com>
2024-12-01 21:57:06 +0300

    dsa python 1

---
+++ main.py
@@ -0,0 +1,17 @@
+list=["red","blue","green","purple","yellow"]
+print(list[0])
+print(" ",list[-1])
+list.append("yellow")
+list.remove("green")
+list.sort()
+list.reverse()
+list.pop(3)
+print(list)
+
+
+
+
+list=[1,3,4,2,0,56,12,-1]
+list.sort()
+list.reverse()
+print(list)
---
+++ main2.py
@@ -0,0 +1,10 @@
+mydict={"name":"kieran","age":29,"grade":12}
+print(mydict)
+print("my name is" ,mydict["name"],"my age is",mydict["age"])
+mydict["age"]=21
+mydict["adress"]="kenya"
+print(mydict)
+mydict.pop("age")
+print(mydict)
+mydict.clear()
+print(mydict)
